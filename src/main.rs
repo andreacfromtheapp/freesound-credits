916275e07aac1d543dbab344673721fe3666686a
gacallea <3269984+gacallea@users.noreply.github.com>
2024-07-22 08:43:42 +0200

    trailing whitespace minor change
    Signed-off-by: gacallea <3269984+gacallea@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn set_credits_header(song_title: &str) -> String {
     format!(
         "## Credits
 
-*{song_title}* includes the following samples from 
+*{song_title}* includes the following samples from
 [Freesound](https://freesound.org). Used under a
 [Creative Commons](https://creativecommons.org)
 [CC-BY](https://creativecommons.org/licenses/by/4.0/) or
