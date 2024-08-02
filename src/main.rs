81e3ae400d61a9fbf5db3853f685aace8c6da19f
gacallea <3269984+gacallea@users.noreply.github.com>
2024-08-02 23:20:38 +0200

    naming
    Signed-off-by: gacallea <3269984+gacallea@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ use std::process;
 
 fn main() {
     let args = Args::parse();
-    let output_filename = set_filename(&args.title);
+    let credits_file = set_filename(&args.title);
 
-    let mut output_file = File::create(&output_filename).unwrap_or_else(|error| {
-        eprintln!("Problem creating '{output_filename}' file: {error}");
+    let mut output = File::create(&credits_file).unwrap_or_else(|error| {
+        eprintln!("Problem creating '{credits_file}' file: {error}");
         process::exit(1);
     });
 
     if args.zola {
         write!(
-            output_file,
+            output,
             "{}",
             set_frontmatter(&args.title, &args.date, &args.artist)
         )
@@ -27,19 +27,19 @@ fn main() {
         });
     }
 
-    write!(output_file, "{}", set_header(&args.title)).unwrap_or_else(|error| {
+    write!(output, "{}", set_header(&args.title)).unwrap_or_else(|error| {
         eprintln!("Problem writing the credits header: {error}");
         process::exit(2);
     });
 
     get_list_of_samples(&args.path).iter().for_each(|line| {
-        write!(output_file, "{}", set_credit(line)).unwrap_or_else(|error| {
+        write!(output, "{}", set_credit(line)).unwrap_or_else(|error| {
             eprintln!("Problem writing the sample credit: {error}");
             process::exit(2);
         });
     });
 
-    writeln!(output_file).unwrap_or_else(|error| {
+    writeln!(output).unwrap_or_else(|error| {
         eprintln!("Problem writing the trailing white line: {error}");
         process::exit(2);
     });
