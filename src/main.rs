bb258255e91aa30d45064d01f11dddb7d47a2a66
gacallea <3269984+gacallea@users.noreply.github.com>
2024-08-02 18:39:19 +0200

    return error on file creation and exit. it also feels very idiomatic now :)
    Signed-off-by: gacallea <3269984+gacallea@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,26 @@ use std::process;
 fn main() {
     let args = Args::parse();
 
-    if let Ok(mut output) = File::create(set_filename(&args.title)) {
-        if args.zola {
-            write!(
-                output,
-                "{}",
-                set_frontmatter(&args.title, &args.date, &args.artist)
-            )
-            .expect("Error: I could not write the Zola frontmatter");
-        }
-
-        write!(output, "{}", set_header(&args.title))
-            .expect("Error: I could not write the credits header");
-
-        get_list_of_samples(&args.path).iter().for_each(|line| {
-            write!(output, "{}", set_credit(line))
-                .expect("Error: I could not write the sample credit");
-        });
-
-        writeln!(output).expect("Error: I could not write the trailing white line");
-    } else {
-        eprintln!("Problem creating the credits output file");
+    let mut output = File::create(set_filename(&args.title)).unwrap_or_else(|error| {
+        eprintln!("Problem creating the credits output file: {error}");
         process::exit(1);
+    });
+
+    if args.zola {
+        write!(
+            output,
+            "{}",
+            set_frontmatter(&args.title, &args.date, &args.artist)
+        )
+        .expect("Error: I could not write the Zola frontmatter");
     }
+
+    write!(output, "{}", set_header(&args.title))
+        .expect("Error: I could not write the credits header");
+
+    get_list_of_samples(&args.path).iter().for_each(|line| {
+        write!(output, "{}", set_credit(line)).expect("Error: I could not write the sample credit");
+    });
+
+    writeln!(output).expect("Error: I could not write the trailing white line");
 }
